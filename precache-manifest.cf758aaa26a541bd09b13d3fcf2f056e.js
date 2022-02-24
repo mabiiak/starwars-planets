3d4133b0b481b4194952c03645dfdbf9d48ec12c
mabiak <mabiane.polniak@gmail.com>
2022-02-23 22:32:04 -0300

    Updates

---
+++ precache-manifest.cf758aaa26a541bd09b13d3fcf2f056e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6416a58553e57108a1a007af0b3564",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "12dcf7f4a4616364ec92",
+    "url": "/starwars-planets/static/css/main.214d61b9.chunk.css"
+  },
+  {
+    "revision": "b325c2e2b3e830345d52",
+    "url": "/starwars-planets/static/js/2.3fa00fd7.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/starwars-planets/static/js/2.3fa00fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dcf7f4a4616364ec92",
+    "url": "/starwars-planets/static/js/main.bd4d4eb6.chunk.js"
+  },
+  {
+    "revision": "d9d282393e64bbdbf77e",
+    "url": "/starwars-planets/static/js/runtime-main.9360541a.js"
+  },
+  {
+    "revision": "2852f8705370b266be525c6c36b6b8b4",
+    "url": "/starwars-planets/static/media/planets.2852f870.jpeg"
+  }
+]);
